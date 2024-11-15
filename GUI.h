41892ca37467688c6543b772ce87f03bc5fb9753
FABU0611 <tanaka.ren.2003.0116@gmail.com>
2024-11-15 15:56:34 +0900

    GUI作成開始

---
+++ GUI.h
@@ -0,0 +1,15 @@
+//GUI.h
+//20106_ìcíÜÅ@ò@
+//24_11_14
+#pragma once
+
+class GUI {
+public:
+	GUI() = delete;
+	virtual ~GUI() {}
+
+	virtual void Init() = 0;
+	virtual void Uninit() = 0;
+	virtual void Update() = 0;
+	virtual void Draw() = 0;
+};
---
+++ G_Button.h
@@ -0,0 +1,10 @@
+//G_Button.h
+//20106_“c’†@˜@
+//24_11_14
+#pragma once
+
+#include "GUI.h"
+
+class Button : public GUI {
+public:
+};
