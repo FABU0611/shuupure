d27b65ab11c4b44f9649ae8239a8f0e3259d8900
FABU0611 <tanaka.ren.2003.0116@gmail.com>
2024-11-21 17:36:57 +0900

    TextManager追加

---
+++ TextManager.cpp
@@ -0,0 +1,12 @@
+//TextManager.h
+//20106_“c’†@˜@
+//24_11_21
+#include "TextManager.h"
+
+void TextManager::Init() {}
+
+void TextManager::Uninit() {}
+
+void TextManager::Update() {}
+
+void TextManager::Draw() {}
---
+++ TextManager.h
@@ -0,0 +1,15 @@
+//TextManager.h
+//20106_“c’†@˜@
+//24_11_21
+#pragma once
+
+class TextManager {
+public:
+	TextManager() {}
+	~TextManager() {}
+
+	void Init();
+	void Uninit();
+	void Update();
+	void Draw();
+};
