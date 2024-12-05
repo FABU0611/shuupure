b105d186dfa7d5b0569696685501314c60d7471b
FABU0611 <tanaka.ren.2003.0116@gmail.com>
2024-12-05 09:08:14 +0900

    GUIクラスで機能

--- G_Button.h
+++ G_Button.h
@@ -2,19 +2,14 @@
 //20106_ìcíÜÅ@ò@
 //24_11_14
 #pragma once
-
 #include "GUI.h"
 
 class Button : public GUI {
 public:
-	Button(const XMFLOAT3& pos, const XMFLOAT3& size);
+	Button(const XMFLOAT3& pos, const XMFLOAT3& size, const wchar_t* texname);
 	~Button() {}
 
 	void Init()override;
-	void Uninit()override;
-	void Update()override;
-	void Draw()override;
 
-	bool IsHovered(const POINT pos) override;
-	bool OnClicked(const int& mousebutton);
+	bool OnClicked(const int& mousebutton) override;
 };
