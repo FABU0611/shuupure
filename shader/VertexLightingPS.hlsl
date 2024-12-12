4c3a1670dc72df679a6971914e3406d7dfa0ac40
FABU0611 <tanaka.ren.2003.0116@gmail.com>
2024-12-12 14:24:42 +0900

    MRT対応

--- shader/VertexLightingPS.hlsl
+++ shader/VertexLightingPS.hlsl
@@ -7,15 +7,14 @@ Texture2D g_Texture : register(t0);
 SamplerState g_SamplerState : register(s0);
 
 
-void main(in PS_IN In, out float4 outDiffuse : SV_Target) {
+void main(in PS_IN In, out PS_OUT Out) {
 
 	if(Material.TextureEnable) {
-		outDiffuse = g_Texture.Sample(g_SamplerState, In.TexCoord);
-		outDiffuse *= In.Diffuse;
+		Out.Out0 = g_Texture.Sample(g_SamplerState, In.TexCoord);
+		Out.Out0 *= In.Diffuse;
 	}
 	else {
-		outDiffuse = In.Diffuse;
+		Out.Out0 = In.Diffuse;
 	}
-	outDiffuse.a = 1.0f;
 }
 
