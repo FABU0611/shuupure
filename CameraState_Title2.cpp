5056923e630a617a4bacc70935367627faccb251
FABU0611 <tanaka.ren.2003.0116@gmail.com>
2025-02-12 17:49:04 +0900

    パラメータ調整

--- CameraState_Title2.cpp
+++ CameraState_Title2.cpp
@@ -20,9 +20,14 @@ void TitleState2::Update() {
 		Manager::GetGaussian()->SetDof({ 0.128f, 0.05f });
 		Manager::GetGaussian()->SetBoke(10.0f);
 		_time = 0.0f;
+		camera->SetPosition({ 0.0f, 25.0f, 30.0f });
 		camera->SetRotation({ -0.76f, -0.8f, 0.6f });
 		camera->SetTarget({ 35.0f, 6.3f, 15.0f });
-		camera->SetLength(30.0f);
+		float vx, vz;
+		vx = camera->GetPosition().x - camera->GetTartgetPos().x;
+		vz = camera->GetPosition().z - camera->GetTartgetPos().z;
+		float length = sqrtf(vx * vx + vz * vz);
+		camera->SetLength(length);
 		camera->ChangeState(new TitleState1(camera));
 	}
 }
